731ca72570e8dec7a74c157f12b8956de33e8b78
Macohi <sinconsistencia@gmail.com>
2026-03-15 01:28:20 -0500

    Basic thing: you can click it and it will add to the score

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,3 +1,24 @@
 import flixel.FlxState;
 
-class PlayState extends FlxState {}
+class PlayState extends FlxState
+{
+	public var theObject:TheObject = new TheObject();
+
+	public var score:Int = 0;
+
+	override function create()
+	{
+		super.create();
+
+		add(theObject);
+		theObject.onLeftClick.add(function()
+		{
+			increment(1);
+		});
+	}
+
+	public function increment(amount:Int)
+	{
+		score += amount;
+	}
+}
