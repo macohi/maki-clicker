2fa8e5d3ed5997ea1e32010e93bc41b7b7e5218c
Macohi <sinconsistencia@gmail.com>
2026-03-15 12:45:20 -0500

    config class functions for different types

--- source/Config.hx
+++ source/Config.hx
@@ -1,3 +1,4 @@
+import haxe.Json;
 import lime.utils.Assets;
 
 using StringTools;
@@ -19,15 +20,38 @@ class Config
 		return Assets.getText(path).split('\n');
 	}
 
-    public function getData(field:String):String
-    {
-        for (line in fileLines)
-        {
-            if (!line.startsWith('$field:')) continue;
+	public function getData(field:String):String
+	{
+		for (line in fileLines)
+		{
+			if (!line.startsWith('$field:'))
+				continue;
 
-            return line.substr('$field:'.length).trim();
-        }
+			return line.substr('$field:'.length).trim();
+		}
 
-        return null;
-    }
+		return null;
+	}
+
+	public function getString(field:String):String
+		return getData(field);
+
+	public function getFloat(field:String):Null<Float>
+		return Std.parseFloat(getString(field));
+
+	public function getInt(field:String):Null<Int>
+		return Std.parseInt(getString(field));
+
+	public function getJSON(field:String):Null<Dynamic>
+	{
+		try
+		{
+			return Json.parse(getString(field));
+		}
+		catch (e)
+		{
+			trace('Config($path) getJSON error: $e');
+			return null;
+		}
+	}
 }
